c7781de435ec0ea320b590df35c5176dab278575
Piotr Przybylak <piotr.przybylak@gmail.com>
2022-06-28 23:36:21 +0200

    Remove redundant code

--- hello.py
+++ hello.py
@@ -4,48 +4,13 @@ import psycopg2
 import psycopg2.extras
 
 connection_string = os.environ.get('DATABASE_URL')
-counter = 0
-
 app = Flask(__name__)
-
-print("aaaa" + str(counter))
 
 
 @app.route("/")
 def hello_world():
     restaurants = execute_select("select name from restaurants")
     return render_template("restaurants.html", restaurants=restaurants)
-
-
-@app.route("/test")
-def test():
-    name = request.args.get("name")
-    print(name)
-    global counter
-    counter += 1
-    # return sklej_htmla(counter)
-    return render_template("example.html", my_counter=counter, name=name)
-
-
-@app.route("/test2", methods=['POST'])
-def test2():
-    name = request.form.get("name")
-    print(name)
-    global counter
-    counter += 1
-    # return sklej_htmla(counter)
-    return render_template("example.html", my_counter=counter, name=name)
-
-
-@app.route("/test2", methods=['GET'])
-def test2a():
-    return "akuku"
-
-
-def sklej_htmla(counter):
-    wygenerowany_htnl = "<h1>Hi!</h1> Counter:  " + str(counter)
-    print(wygenerowany_htnl)
-    return wygenerowany_htnl
 
 
 def execute_select(statement, variables=None, fetchall=True):
